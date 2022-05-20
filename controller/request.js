2ba1f27f40eba1e4f580c234d4213f18a22a969a
Elkin Guerrero <elkin-951011@hotmail.com>
2022-05-20 14:19:04 -0500

    update

--- controller/request.js
+++ controller/request.js
@@ -9,7 +9,7 @@ router.post('/test', async (req, res) => {
     var codigo_unico = Math.random().toString(30).slice(-15).replace(/[.]/g,"_");
     var id_test = req.body.id_1;
     var result_final = [];
-    var intent_petition = 0;
+    var intent_petition, end_process;
 
     const service = new chrome.ServiceBuilder('chromedriver/chromedriver');
     const driver = new Builder().forBrowser('chrome').setChromeService(service).build();
@@ -30,6 +30,8 @@ router.post('/test', async (req, res) => {
         driver.get(req.body.url).then(async function () {
             for( var i=0; i<actions.length; i++ ){
                 intent_petition = 0;
+                end_process = false;
+                console.log( actions[i] )
                 if( await f_query( actions[i].vars, actions[i].action, actions[i].time ) == 0 ){
                     i=actions.length;
                 }
@@ -47,87 +49,92 @@ router.post('/test', async (req, res) => {
     
     function f_query( vars, action, time ) {
         time = !isNaN(!time) && time != "" && time != undefined ? parseInt(time) : 1000 ;
-    
         return new Promise(resolve => {
-            if( vars != undefined && vars.class == 'alert_confirm' ){
-                setTimeout(function(){
-                    driver.switchTo().alert().accept();
-                    resolve(1);
-                }, time);
-            }else if( action != 'p' ){
-                arr_query.push(
-                    setInterval(async function () {
-                        
-                        await driver.findElement(object_class(vars.class)).then(async function (webElement) {
-                            if( action == "c" )
-                                await driver.findElement(object_class(vars.class)).click()
-                            else if( action == "w" ){
-                                await driver.findElement(object_class(vars.class)).clear();
-                                await driver.findElement(object_class(vars.class)).sendKeys(vars.text);
-                            }
-    
-                            driver.takeScreenshot().then(function(data){
-                                var base64Data = data.replace(/^data:image\/png;base64,/,"")
-                                result_final.push({
-                                    "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
-                                    "id_test_unique": codigo_unico,
-                                    "id_test":id_test,
-                                    "action": `${btoa( `{ "vars": ${vars}, "action":${action}, "time":${time} },`)}`,
-                                    "response": "ok",
-                                    "details": "",
-                                    "image": btoa(base64Data),
-                                    "registration_date": req.body.fecha,
-                                    "status": "1",
+            
+                if( vars != undefined && vars.class == 'alert_confirm' ){
+                    setTimeout(function(){
+                        driver.switchTo().alert().accept();
+                        resolve(1);
+                    }, time);
+                }else if( action != 'p' ){
+                    arr_query.push(
+                        setInterval(async function () {
+                            if( !end_process ){
+                                await driver.findElement(object_class(vars.class)).then(async function (webElement) {
+                                    end_process = true;
+                                    if( action == "c" )
+                                        await driver.findElement(object_class(vars.class)).click()
+                                    else if( action == "w" ){
+                                        await driver.findElement(object_class(vars.class)).clear();
+                                        await driver.findElement(object_class(vars.class)).sendKeys(vars.text);
+                                    }
+            
+                                    driver.takeScreenshot().then(function(data){
+                                        var base64Data = data.replace(/^data:image\/png;base64,/,"")
+                                        result_final.push({
+                                            "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
+                                            "id_test_unique": codigo_unico,
+                                            "id_test":id_test,
+                                            "action": `${btoa( `{ "vars": ${JSON.stringify(vars)}, "action":${action}, "time":${time} },`)}`,
+                                            "response": "ok",
+                                            "details": "",
+                                            "image": btoa(base64Data),
+                                            "registration_date": req.body.fecha,
+                                            "status": "1",
+                                        });
+                                        resolve(1);
+                                    });
+                                                            
+                                    clearInterval(arr_query[s_clase.indexOf(vars.class)]);
+                                    s_clase[s_clase.indexOf(vars.class)] = undefined;
+                                }, function (err) {
+                                    if( err.name != "NoSuchElementError" )
+                                        console.log(err.name)
                                 });
+        
+                                if( intent_petition > 10 ){
+                                    end_process = true;
+                                    clearInterval(arr_query[s_clase.indexOf(vars.class)]);
+                                    result_final.push({
+                                        "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
+                                        "id_test_unique": codigo_unico,
+                                        "id_test":id_test,
+                                        "action": `${btoa( `{ "vars": ${JSON.stringify(vars)}, "action":${action}, "time":${time} },`)}`,
+                                        "response": `Failed`,
+                                        "details": `class or id ${vars.class} not found`,
+                                        "image": "",
+                                        "registration_date": req.body.fecha,
+                                        "status": "1",
+                                    });
+                                    resolve(0);
+                                }
+                                intent_petition++;
+                            }else{
                                 resolve(1);
-                            });
-                                                    
-                            clearInterval(arr_query[s_clase.indexOf(vars.class)]);
-                            s_clase[s_clase.indexOf(vars.class)] = undefined;
-                        }, function (err) {
-                            if( err.name != "NoSuchElementError" )
-                                console.log(err.name)
-                        });
-
-                        if( intent_petition > 10 ){
-                            clearInterval(arr_query[s_clase.indexOf(vars.class)]);
+                            }
+                        }, time)
+                    )
+                
+                    s_clase.push(vars.class);
+                }else{
+                    setTimeout(function(){
+                        driver.takeScreenshot().then(function(data){
+                            var base64Data = data.replace(/^data:image\/png;base64,/,"")
                             result_final.push({
                                 "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
-                                "id_unique": codigo_unico,
+                                "id_test_unique": codigo_unico,
                                 "id_test":id_test,
-                                "action": `${btoa( `{ "vars": ${vars}, "action":${action}, "time":${time} },`)}`,
-                                "response": `Failed`,
-                                "details": `class or id ${vars.class} not found`,
-                                "image": "",
+                                "action": `${btoa( `{ "vars": "", "action":${action}, "time":${time} },`)}`,
+                                "response": "ok",
+                                "details": "",
+                                "image": btoa(base64Data),
                                 "registration_date": req.body.fecha,
                                 "status": "1",
                             });
-                            resolve(0);
-                        }
-                        intent_petition++;
-                    }, time)
-                )
-            
-                s_clase.push(vars.class);
-            }else{
-                setTimeout(function(){
-                    driver.takeScreenshot().then(function(data){
-                        var base64Data = data.replace(/^data:image\/png;base64,/,"")
-                        result_final.push({
-                            "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
-                            "id_unique": codigo_unico,
-                            "id_test":id_test,
-                            "action": `${btoa( `{ "vars": ${vars}, "action":${action}, "time":${time} },`)}`,
-                            "response": "ok",
-                            "details": "",
-                            "image": btoa(base64Data),
-                            "registration_date": req.body.fecha,
-                            "status": "1",
+                            resolve(1);
                         });
-                        resolve(1);
-                    });
-                }, time);
-            }
+                    }, time);
+                }
         });
     }
     
