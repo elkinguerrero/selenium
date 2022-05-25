f8fe58c3a5c8ebb812a1da64e4671a34efcea4dc
Elkin Guerrero <elkin-951011@hotmail.com>
2022-05-25 16:41:31 -0500

    update

--- controller/request.js
+++ controller/request.js
@@ -33,7 +33,7 @@ router.post('/test', async (req, res) => {
                 intent_petition = 0;
                 end_process = false;
                 console.log( actions[i] )
-                if( await f_query( actions[i].vars, actions[i].action, actions[i].time, (i+1)) == 0 ){
+                if( await f_query( actions[i], (i+1)) == 0 ){
                     i=actions.length;
                 }
             }
@@ -48,23 +48,23 @@ router.post('/test', async (req, res) => {
         });
     }
     
-    function f_query( vars, action, time, cont_action ) {
-        time = !isNaN(!time) && time != "" && time != undefined ? parseInt(time) : 0 ;
+    function f_query( actions, cont_action ) {
+        actions.time = !isNaN(!actions.time) && actions.time != "" && actions.time != undefined ? parseInt(actions.time) : 0 ;
         return new Promise(resolve => {
-                if( vars != undefined && (vars.class == 'alert_confirm' || vars.class == 'alert_deny') ){
-                    time = time<2000 ? 2000 : time ;
-                    s_clase.push(vars.class);
+                if( actions.vars != undefined && (actions.vars.class == 'alert_confirm' || actions.vars.class == 'alert_deny') ){
+                    actions.time = actions.time<2000 ? 2000 : actions.time ;
+                    s_clase.push(actions.vars.class);
                     arr_query.push(
                         async function () {
-                            await timeout(time);
+                            await timeout(actions.time);
 
                             details = "";
                             if( !end_process ){                                
                                 driver.switchTo().alert().then(async function(e) {
                                     details = await e.getText();
-                                    if( vars.class == 'alert_confirm' )
+                                    if( actions.vars.class == 'alert_confirm' )
                                         driver.switchTo().alert().accept();
-                                    else if( vars.class == 'alert_deny' )
+                                    else if( actions.vars.class == 'alert_deny' )
                                         driver.switchTo().alert().dismiss();
 
                                         driver.switchTo().alert().dismiss();
@@ -76,7 +76,7 @@ router.post('/test', async (req, res) => {
                                             "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
                                             "id_test_unique": codigo_unico,
                                             "id_test":id_test,
-                                            "action": `${btoa( `{ "vars": ${JSON.stringify(vars)}, "action":${action}, "time":${time} },`)}`,
+                                            "action": `${btoa( JSON.stringify(actions) )}`,
                                             "response": "ok",
                                             "details": details,
                                             "image": "",
@@ -84,66 +84,68 @@ router.post('/test', async (req, res) => {
                                             "status": "1",
                                             "id_num_test":cont_action,
                                         });
-                                        s_clase[s_clase.indexOf(vars.class)] = undefined;
+                                        s_clase[s_clase.indexOf(actions.vars.class)] = undefined;
                                         resolve(1);
-                                        
                                     }
                                 }, async function (err) {
-                                    if( !end_process )
-                                        console.log( `---------------------------\nNo se encontro el elemento ${vars.class} se intentara buscarlo de nuevo` )
+                                    if( !end_process ){
+                                        if( intent_petition > 9 ){
+                                            console.log( `---------------------------\nclass or id ${actions.vars.class} not found` )
+                                            end_process = true;
+                                            result_final.push({
+                                                "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
+                                                "id_test_unique": codigo_unico,
+                                                "id_test":id_test,
+                                                "action": `${btoa( JSON.stringify(actions) )}`,
+                                                "response": `Failed`,
+                                                "details": `class or id ${actions.vars.class} not found`,
+                                                "image": "",
+                                                "registration_date": req.body.fecha,
+                                                "status": "1",
+                                                "id_num_test":cont_action,
+                                            });
+                                            resolve(0);
+                                        }else if( s_clase.indexOf(actions.vars.class) != -1 ){
+                                            console.log( `---------------------------\nNo se encontro el elemento ${actions.vars.class} se intentara buscarlo de nuevo` )
+                                            intent_petition++;
+                                            arr_query[s_clase.indexOf(actions.vars.class)]()
+                                        }
+                                    }
                                 });
-        
-                                if( intent_petition > 9 ){
-                                    console.log( `---------------------------\nclass or id ${vars.class} not found` )
-                                    end_process = true;
-                                    result_final.push({
-                                        "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
-                                        "id_test_unique": codigo_unico,
-                                        "id_test":id_test,
-                                        "action": `${btoa( `{ "vars": ${JSON.stringify(vars)}, "action":${action}, "time":${time} },`)}`,
-                                        "response": `Failed`,
-                                        "details": `class or id ${vars.class} not found`,
-                                        "image": "",
-                                        "registration_date": req.body.fecha,
-                                        "status": "1",
-                                        "id_num_test":cont_action,
-                                    });
-                                    resolve(0);
-                                }else if( s_clase.indexOf(vars.class) != -1 ){
-                                    intent_petition++;
-                                    arr_query[s_clase.indexOf(vars.class)]()
-                                }
                             }else{
-                                resolve(1);
+                                resolve(0);
                             }
                         }
                     )
                 
                     async function run(){
                         //Se pausa la aplicacion por un tiempo mientras por tiempo definido por el usuario
-                        arr_query[s_clase.indexOf(vars.class)]()
+                        arr_query[s_clase.indexOf(actions.vars.class)]()
                     }
                     run()
-                }else if( action != 'p' ){
-                    s_clase.push(vars.class);
+                }else if( actions.action != 'p' ){
+                    s_clase.push(actions.vars.class);
                     arr_query.push(
                         async function () {
                             if( !end_process ){
                                 //Se hace uso de la funcion wait para comprobar que el elemento realmente existe
-                                await driver.wait(until.elementLocated(By.css(vars.class)),3000).then(async function (webElement) {
+                                await driver.wait(until.elementLocated(By.css(actions.vars.class)),3000).then(async function (webElement) {
                                     //Se busca el elemento esto es necesario por si la pagina cambia y el elemento no existe
-                                    await driver.findElement(object_class(vars.class)).then(async function (webElement) {
+                                    await driver.findElement(object_class(actions.vars.class)).then(async function (webElement) {
                                         end_process = true;
                                         details = "";
-                                        if( action == "c" )
-                                            await driver.findElement(object_class(vars.class)).click()
-                                        else if( action == "gt" )
-                                            details = await driver.findElement(object_class(vars.class)).getText();
-                                        else if( action == "gv" )
-                                            details = await driver.findElement(object_class(vars.class)).getAttribute("value");
-                                        else if( action == "w" ){
-                                            await driver.findElement(object_class(vars.class)).clear();
-                                            await driver.findElement(object_class(vars.class)).sendKeys(vars.text);
+                                        if( actions.action == "c" )
+                                            await driver.findElement(object_class(actions.vars.class)).click()
+                                        else if( actions.action == "gt" )
+                                            details = await driver.findElement(object_class(actions.vars.class)).getText();
+                                        else if( actions.action == "gv" )
+                                            details = await driver.findElement(object_class(actions.vars.class)).getAttribute("value");
+                                        else if( actions.action == "w" ){
+                                            await driver.findElement(object_class(actions.vars.class)).clear();
+                                            await driver.findElement(object_class(actions.vars.class)).sendKeys(actions.vars.text);
+                                        }else if( actions.action == "f" ){
+                                            details = await driver.findElement(object_class(actions.vars.class)).getAttribute(actions.attribute);
+                                            details = `Coincidencias encontradas: ${details.split(actions.find).length-1}`;
                                         }
                 
                                         await driver.takeScreenshot().then(function(data){
@@ -152,7 +154,7 @@ router.post('/test', async (req, res) => {
                                                 "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
                                                 "id_test_unique": codigo_unico,
                                                 "id_test":id_test,
-                                                "action": `${btoa( `{ "vars": ${JSON.stringify(vars)}, "action":${action}, "time":${time} },`)}`,
+                                                "action": `${btoa( JSON.stringify(actions) )}`,
                                                 "response": "ok",
                                                 "details": details,
                                                 "image": btoa(base64Data),
@@ -162,48 +164,48 @@ router.post('/test', async (req, res) => {
                                             });
                                             resolve(1);
                                         });
-                                        s_clase[s_clase.indexOf(vars.class)] = undefined;
+                                        s_clase[s_clase.indexOf(actions.vars.class)] = undefined;
                                     }, function (err) {
                                         if( err.name == "NoSuchElementError" ){
-                                            console.log( `---------------------------\nNo se encontro el elemento ${vars.class} se intentara buscarlo de nuevo` )
+                                            console.log( `---------------------------\nNo se encontro el elemento ${actions.vars.class} se intentara buscarlo de nuevo` )
                                         }else{
                                             console.log(`---------------------------\n${err.name}`)
                                         }
                                     });
                                 }, function (err) {
-                                    console.log( `---------------------------\nNo se encontro el elemento ${vars.class} se intentara buscarlo de nuevo` )
+                                    console.log( `---------------------------\nNo se encontro el elemento ${actions.vars.class} se intentara buscarlo de nuevo` )
+
+                                    if( intent_petition > 10 ){
+                                        console.log( `---------------------------\nclass or id ${actions.vars.class} not found` )
+                                        end_process = true;
+                                        result_final.push({
+                                            "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
+                                            "id_test_unique": codigo_unico,
+                                            "id_test":id_test,
+                                            "action": `${btoa( JSON.stringify(actions) )}`,
+                                            "response": `Failed`,
+                                            "details": `class or id ${actions.vars.class} not found`,
+                                            "image": "",
+                                            "registration_date": req.body.fecha,
+                                            "status": "1",
+                                            "id_num_test":cont_action,
+                                        });
+                                        resolve(0);
+                                    }else if( s_clase.indexOf(actions.vars.class) != -1 ){
+                                        intent_petition++;
+                                        arr_query[s_clase.indexOf(actions.vars.class)]()
+                                    }
                                 });
-        
-                                if( intent_petition > 10 ){
-                                    console.log( `---------------------------\nclass or id ${vars.class} not found` )
-                                    end_process = true;
-                                    result_final.push({
-                                        "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
-                                        "id_test_unique": codigo_unico,
-                                        "id_test":id_test,
-                                        "action": `${btoa( `{ "vars": ${JSON.stringify(vars)}, "action":${action}, "time":${time} },`)}`,
-                                        "response": `Failed`,
-                                        "details": `class or id ${vars.class} not found`,
-                                        "image": "",
-                                        "registration_date": req.body.fecha,
-                                        "status": "1",
-                                        "id_num_test":cont_action,
-                                    });
-                                    resolve(0);
-                                }else if( s_clase.indexOf(vars.class) != -1 ){
-                                    intent_petition++;
-                                    arr_query[s_clase.indexOf(vars.class)]()
-                                }
                             }else{
-                                resolve(1);
+                                resolve(0);
                             }
                         }
                     )
                 
                     async function run(){
                         //Se pausa la aplicacion por un tiempo mientras por tiempo definido por el usuario
-                        await timeout(time);
-                        arr_query[s_clase.indexOf(vars.class)]()
+                        await timeout(actions.time);
+                        arr_query[s_clase.indexOf(actions.vars.class)]()
                     }
                     run()
                 }else{
@@ -214,7 +216,7 @@ router.post('/test', async (req, res) => {
                                 "id": Math.random().toString(30).slice(-15).replace(/[.]/g,"_"),
                                 "id_test_unique": codigo_unico,
                                 "id_test":id_test,
-                                "action": `${btoa( `{ "vars": "", "action":${action}, "time":${time} },`)}`,
+                                "action": `${btoa( JSON.stringify(actions) )}`,
                                 "response": "ok",
                                 "details": "",
                                 "image": btoa(base64Data),
@@ -224,7 +226,28 @@ router.post('/test', async (req, res) => {
                             });
                             resolve(1);
                         });
-                    }, time);
+                    }, actions.time);
+
+                    // setTimeout(async function(){
+                    //     //Se hace uso de la funcion wait para comprobar que el elemento realmente existe
+                    //     await driver.wait(until.elementLocated(By.css("#glcanvas")),3000).then(async function (webElement) {
+                    //         //Se busca el elemento esto es necesario por si la pagina cambia y el elemento no existe
+                    //         await driver.findElement(object_class("#glcanvas")).then(async function (webElement) {
+                    //             console.log( "-------" )
+                    //             // console.log( 1 
+                    //             var element = driver.findElement(object_class("#glcanvas"));
+
+                    //             const actions = driver.actions({async: true});
+                    //             // Performs mouse move action onto the element
+                    //             await actions.move({x:10,y:10}).pause(3000).perform();
+                    //             // actions.move_to_element_with_offset(driver.find_element_by_tag_name('body'), 0,0))
+
+                    //             // driver.swipe(540, 905, 540, 905, 50);
+                    //         });
+                    //     }, function (err) {
+                    //         console.log( `---------------------------\nNo se encontro el elemento ${actions.vars.class} se intentara buscarlo de nuevo` )
+                    //     });
+                    // }, 10000);
                 }
         });
     }
